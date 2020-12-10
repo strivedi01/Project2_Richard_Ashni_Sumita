9380871e3e20027e954375c5f97fecbdd08bba5b
Sumita Trivedi <strivedi_nj@yahoo.com>
2020-12-10 09:14:03 -0500

    added load_data_into_database.ipynb

--- notebooks/app.py
+++ notebooks/app.py
@@ -1,86 +1,85 @@
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
+#importnecessarylibraries
+	#from models import create_classes
+	
+import os
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
+from sqlalchemy.ext.automap import automap_base
+from flask import (
+	Flask,
+	render_template,
+	jsonify,
+	request,
+	redirect)
+	
 
+	#################################################
+	# Flask Setup
+	#################################################
+app = Flask(__name__)
+	
 
 #################################################
-# Database Setup
+	# Database Setup
 #################################################
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', '') or "postgresql://postgres:abc123abc@localhost:5432/covid_data"
 
-engine = create_engine("sqlite:///my_db.sqlite")
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False	
+
+db = SQLAlchemy(app)	
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])	
+
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-Base.classes.keys()
 # Save reference to the table
-Covid = Base.classes.covid_unemployment_data
-
-
-#################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
-
+covidj = Base.classes.covid_data
+	
 
 #################################################
-# Flask Routes
+# Endpoints Setup
 #################################################
-
+# create route that renders index.html template
 @app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/County_Month<br/>"
-        f"/api/v1.0/Unemployed<br/>"
-        f"/api/v1.0/Covid_Cases<br/>"
-    )
+def home():
+	return render_template("index.html")
+	
 
+# create endpoint that shares data in json format
+@app.route("/api/covidj")
+def covidj():
+	# Query data from Covid table in db
+	results = db.session.query(covidj.id, covidj.County_Month, covidj.countyFIPS, covidj.County,
+	                            covidj.Covid_Cases, covidj.year, covidj.month, covidj.Month_c, covidj.County_FIPS, 
+                                covidj.Month_Year,  covidj.Employed, covidj.Unemployed, covidj.Unemployment_Rate).all()
+	    
+	# put data into dictionary format that can be jsonified
+	covid_data = []
+	for result in results:
+	    covid_data.append({
+	        "id":result[1],
+	        "County_Month":result[2],
+	        "countyFIPS":result[3],
+	        "County":result[4],                
+	        "Covid_Cases":result[6],
+	        "year":result[7],
+	        "month":result[8],
+	        "Month_c":result[9],
+            "County_FIPS":result[10],
+            "Month_Year":result[16],
+            "Employed":result[18],
+            "Unemployed":result[19],
+            "Unemploment_Rate":result[20]})
+	    
+	    # display jsonified data on the dom
+	return jsonify(covid_data)
+	    
+	
 
-@app.route("/api/v1.0/county")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+if __name__ == "__main__":
+	app.run()
 
-    """Return a list of all county names"""
-    # Query all passengers
-    results = session.query(Covid.id).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
-
-
-@app.route("/api/v1.0/cases")
-def counties():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of counties including the Covid_Cases, Unemployed, UnemploymentRate"""
-    # Query all counties
-    results = session.query(Covid.Covid_Cases, Covid.Unemployed, Covid.UnemploymentRate).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_counties = []
-    for Cases, Unemployed, UnemploymentRate in results:
-        county_dict = {}
-        county_dict["Covid_Cases"] = Cases
-        county_dict["Unemployed"] = Unemployed
-        county_dict["UnemploymentRate"] = UnemploymentRate
-        all_counties.append(county_dict)
-
-    return jsonify(all_counties)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
